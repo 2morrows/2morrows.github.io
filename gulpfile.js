a78d3d88275057b0bdb8c7252a47e14293c4b261
mmorrow <mmorrow@dclibraries.org>
2017-11-30 08:38:12 -0700

    caught up with downloads

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,13 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function(){
-    console.log("Hooray gulp task")
-});
-gulp.task('html', function(){
-    console.log("Hooray gulp html task")
-});
 
-gulp.task('watch', function(){
-   watch('./app/index.html',function(){
-   gulp.start('html');
-});
- });
- 
+
+
+
+
+
+
+
+
 
